9edf0d810d4ac78cf9c1536c6ffc684efedd10e2
Rk4609 <rakeshjangid8742@gmail.com>
2026-05-11 17:13:51 +0530

    snake_project

---
+++ Chapter7/snake_project.py
@@ -0,0 +1,37 @@
+# snake water gun
+import random
+'''
+ 1 for snake
+ -1 for water
+ 0 for gun
+
+'''
+
+computer = random.choice([-1,0,1])
+youStr= input("Enter your choice :")
+youDict = {"s":1,"w":-1,"g":0}
+reverseDict={1:"snake",-1:"water",0:"gun"}
+
+you= youDict[youStr]
+
+print(f"You chose {reverseDict[you]}\nwcomputer chose {reverseDict[computer]}")
+
+if(computer==you):
+    print("its a draw..")
+
+else:
+    if(computer==-1 and you==1):
+        print("You win!..")
+    elif(computer==-1 and you==0):
+        print("You win!..")
+    elif(computer==1 and you==-1):
+        print("You win!..")
+    elif(computer==1 and you==0):
+        print("You win!..")
+    elif(computer==0 and you==-1):
+        print("You win!..")
+    elif(computer==0 and you==1):
+        print("You win!..")
+
+    else:
+        print("Something went wrong..")
