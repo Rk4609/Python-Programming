f2d101d258bc35989cee17451478525070739e3e
Rk4609 <rakeshjangid8742@gmail.com>
2026-05-13 10:33:12 +0530

    Add two dict

--- Chapter4/01_dictionary.py
+++ Chapter4/01_dictionary.py
@@ -9,3 +9,12 @@ marks ={
 
 print(marks,type(marks))  # class <dict>
 print(marks["Harish"])
+
+
+# Two dictionary add
+dict1 = {"Harish":77,"Rakesh":99}
+dict2= {"Krish":66,"Sanjay":85}
+
+dictMerge = dict1 | dict2
+
+print(dictMerge)
