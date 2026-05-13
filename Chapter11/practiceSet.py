630e7e927b52026fb9176343c957eeef80640878
Rk4609 <rakeshjangid8742@gmail.com>
2026-05-13 13:02:21 +0530

    Add practiceSet.py with file handling, list comprehension, and error handling examples

---
+++ Chapter11/practiceSet.py
@@ -0,0 +1,61 @@
+# 1.Write a program to open three files 1.txt, 2.txt and 3.txt. If any of these files are not
+# present, a message without exiting the program must be printed prompting the same.
+
+try:
+    with open("1.txt","r") as f:
+         print(f.read())
+except Exception as e:
+     print(e)
+
+try:
+    with open("Chapter11/2.txt","r") as f:
+         print(f.read())
+except Exception as e:
+     print(e)
+
+try:
+    with open("3.txt","r") as f:
+         print(f.read())
+except Exception as e:
+     print(e)
+
+print("Thank you")
+
+
+#--------------------------------------------------------------------------------------
+# 2. Write a program to print third, fifth and seventh element from a list using enumerate
+# function.
+
+l =[1,2,3,4,5,6,7,8,9,10]
+
+for i,item in enumerate(l):
+    if(i == 2 or i == 4 or i == 6):
+        print(item)
+
+#--------------------------------------------------------------------------------------
+# 3. Write a list comprehension to print a list which contains the multiplication table of a user
+# entered number.
+
+n = int(input("Enter a number :"))
+table = [i*n for i in range(1,11)]
+print(table)
+
+#--------------------------------------------------------------------------------------
+# 4. Write a program to display a/b where a and b are integers. If b=0, display infinite by
+# handling the ‘ZeroDivisionError’ .
+
+try:
+    a = int(input("Enter a :"))
+    b = int(input("Enter b  :"))
+    print(a/b)
+
+except ZeroDivisionError as v:
+    print("infinite")
+
+#--------------------------------------------------------------------------------------
+# 5. Store the multiplication tables generated in problem 3 in a file named Tables.txt .
+
+n = int(input("Enter a number :"))
+table = [i*n for i in range(1,11)]
+with open("Chapter11/tables.txt","a") as f:
+    f.write(f"table of {n} : {str(table)} \n")
