157f91768012888e7b2315a1a89ccaf2064232a7
Rk4609 <rakeshjangid8742@gmail.com>
2026-05-13 12:08:19 +0530

    Add example of list comprehensions for squaring numbers

---
+++ Chapter11/11_list_comprehensions.py
@@ -0,0 +1,12 @@
+
+myList = [1 ,4 ,5 ,3 ,9, 11]
+
+squareList =[]
+
+# for item in myList:
+#     squareList.append(item*item)
+# print(squareList)
+
+# using list comprehensions
+squareList=[i*i for i in myList]
+print(squareList)
